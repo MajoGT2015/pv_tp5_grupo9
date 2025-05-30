ed0733f1634a124367d566b9f23b20a043140096
luivilte <vilteluis665@gmail.com>
2025-05-30 17:55:59 -0300

    creacion_de_navbar_y_home

---
+++ Proyecto_tp5_pv/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenidos al Sistema de Gestión de Alumnos</h1>
+      <p>Utilice el menú para navegar por la aplicación.</p>
+    </div>
+  );
+};
+
+export default Home;
