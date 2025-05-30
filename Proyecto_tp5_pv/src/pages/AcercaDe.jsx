d007b22166b3b4b077296bd3a5bff24394b3def3
ivan1julio <julioivanjulio34@gmail.com>
2025-05-30 18:29:59 -0300

    dotos de page

---
+++ Proyecto_tp5_pv/src/pages/AcercaDe.jsx
@@ -0,0 +1,26 @@
+const AcercaDe = () => {
+  return (
+    <div>
+      <h2>Acerca de esta aplicación</h2>
+      <p>Esta aplicación fue desarrollada como parte del Trabajo Práctico N.º 5 de la materia Programación Visual.</p>
+
+      <h3>Integrantes del grupo</h3>
+      <ul>
+        <li>Nombre Apellido GitHub: <a href="https://github.com/usuario1" target="_blank">usuario1</a></li>
+        <li>Nombre Apellido GitHub: <a href="https://github.com/usuario2" target="_blank">usuario2</a></li>
+      </ul>
+
+      <h3>Tecnologías utilizadas</h3>
+      <ul>
+        <li>React + Vite</li>
+        <li>React Router DOM</li>
+        <li>JSX</li>
+        <li>Eventos en React</li>
+        <li>JavaScript moderno</li>
+        <li>(Opcional) TailwindCSS / Material UI</li>
+      </ul>
+    </div>
+  );
+};
+
+export default AcercaDe;
